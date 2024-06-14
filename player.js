105d4ff68ab60c209c03a283e922a8eb1f4ac0dc
Q3nfQ <bender1423@hotmail.com>
2024-06-14 07:29:31 +0300

    Update player.js

--- player.js
+++ player.js
@@ -4,9 +4,9 @@ const { queueNames } = require("./commands/play");
 
 function initializePlayer(client) {
     const nodes = [
-              {
+            {
             host: "lava-v4.ajieblogs.eu.org",
-            password: "https://dsc.gg/b-b",
+            password: "https://dsc.gg/ajidevserver",
             port: 443,
             secure: true
         },
